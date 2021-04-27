6d0d5cb999005109b2ead0d20f9718f4e579a967
josefelipenogueira <josefelipenogueira.90@gmail.com>
2021-04-26 21:25:37 -0300

    etapa1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,73 @@
-Login
+@extends('template.estilo')
+@section('content')
+<title></title>
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Bem Vindo(a)</h1>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Entre com o E-mail...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Senha">
+                                        </div>
+                                        
+                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                            Entrar
+                                        </a>
+                                        <hr>
+                                        <p>Para os testes use as credências a seguir: </p>
+                                        <p>Nível Administrador <br> E-mail: adm@teste.com e Senha mundonovo2021</p>
+                                        <p>Nível Supervisor <br> E-mail: super@teste.com e Senha mundonovo2021</p>
+                                        <p>Nível Funcionário <br> E-mail: func@teste.com e Senha mundonovo2021</p>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Esqueceu a senha?</a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+@endsection
